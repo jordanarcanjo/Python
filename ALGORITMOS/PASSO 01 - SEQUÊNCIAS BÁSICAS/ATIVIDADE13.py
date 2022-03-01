a59c7acabc96e6e1d97b89024a9612dae8695414
jordanaarcanjo <jordanatuelho@gmail.com>
2022-02-28 23:50:12 -0300

    ATIVIDADE 13
    Faça um algoritmo que leia o salário de um funcionário, calcule e mostre o
    seu novo salário, com 15% de aumento.

---
+++ ALGORITMOS/PASSO 01 - SEQUÊNCIAS BÁSICAS/ATIVIDADE13.py
@@ -0,0 +1,7 @@
+salario = float(input('Digite seu salario atual:'))
+
+aumento= (salario + (salario*15/100))
+
+print('Seu salario anterior é de {}:'. format(salario)) 
+print(' Seu novo salario possui o valor total de : {}'. format(aumento))
+
