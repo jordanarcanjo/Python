f8cdf9398038e0bc3187c8311b182b4bb1352076
Jordana Coelho <61910096+jordanarcanjo@users.noreply.github.com>
2022-03-01 00:04:14 -0300

    ATIVIDADE 03
     Crie um programa que leia o nome e o salário de um funcionário, mostrando no
    final uma mensagem.
    Ex:
    Nome do Funcionário: Maria do Carmo
    Salário: 1850,45
    O funcionário Maria do Carmo tem um salário de R$1850,45 em Junho.

---
+++ ALGORITMOS/ATIVIDADE3.py
@@ -0,0 +1,5 @@
+funcionario=input('Digite seu nome:')
+salario=float('Digite o valor atual de se salario:')
+mes=input('Digite seu mês atual:')
+
+print('O funcionário {} tem um salário de {}} em {}.'.format(funcionario,salario.mes))
