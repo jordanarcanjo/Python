a5366f420814608cd49d9a0a3e0febe5a6f03312
Jordana Coelho <61910096+jordanarcanjo@users.noreply.github.com>
2022-03-01 00:08:10 -0300

    Delete ATIVIDADE11.py

--- ALGORITMOS/ATIVIDADE11.py
+++
@@ -1,7 +0,0 @@
-a= int(input('Digite o valor de A:'))
-b= int(input('Digite o valor de B:'))
-c= int(input('Digite o valor de C:'))
-
-delta= (b*b)-(4*a*b)
-
-print('O valor de Delta é de: {}'.format(delta))
