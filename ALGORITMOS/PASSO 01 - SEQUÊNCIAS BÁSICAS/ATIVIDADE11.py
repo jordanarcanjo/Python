17844aa1c9e446ff0cd97ac61981e429e0b1d123
jordanaarcanjo <jordanatuelho@gmail.com>
2022-02-28 23:49:40 -0300

    ATIVIDADE 11
    Crie um programa que leia o preço de um produto, calcule e mostre o seu
    PREÇO PROMOCIONAL, com 5% de desconto.

---
+++ ALGORITMOS/PASSO 01 - SEQUÊNCIAS BÁSICAS/ATIVIDADE11.py
@@ -0,0 +1,7 @@
+a= int(input('Digite o valor de A:'))
+b= int(input('Digite o valor de B:'))
+c= int(input('Digite o valor de C:'))
+
+delta= (b*b)-(4*a*b)
+
+print('O valor de Delta é de: {}'.format(delta))
