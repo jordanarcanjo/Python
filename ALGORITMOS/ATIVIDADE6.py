1dffb6f9ff2154ae74f9da0aacf6d7c0ef8e3034
Jordana Coelho <61910096+jordanarcanjo@users.noreply.github.com>
2022-03-01 00:05:25 -0300

    ATIVIDADE 06
    Faça um programa que leia um número inteiro e mostre o seu antecessor e seu
    sucessor.
    Ex:
    Digite um número: 9
    O antecessor de 9 é 8
    O sucessor de 9 é 10

---
+++ ALGORITMOS/ATIVIDADE6.py
@@ -0,0 +1,10 @@
+print('Descobrir o valor antecessor e sucessor!')
+num= int(input('Digite um numero:'))
+
+antes = (num-1)
+
+print('O antecessor de {} é : {}'.format(num,antes))
+
+suces = (num+1)
+
+print('O sucessor de {} é: {}'.format(num,suces))
